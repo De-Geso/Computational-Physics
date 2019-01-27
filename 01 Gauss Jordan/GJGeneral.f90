c16bb97dda01a46a09fbd3fe0954554290ab119d
PHYS395 - Computational Physics Sandbox <phys395@sandbox.phys.sfu.ca>
2019-01-27 00:32:26 -0800

    Completed Gauss Jordan elimination as GJGeneral. Completed approximation of a function using Chebyshev Polynomials and Gauss Jordan as GJChebyshev. I need to update the comments for both

---
+++ 01 Gauss Jordan/GJGeneral.f90
@@ -0,0 +1,86 @@
+! Compile with: gfortran -O3 -fdefault-real-8 GaussJordan.f90
+
+program GaussianJordanian
+implicit none
+
+! Parameters for question #1
+integer, parameter :: n = 3
+real A(n,n), B(n)
+
+! Test and make sure that the Gauss-Jordan Elimination works.
+! Matrix to invert
+A(1,:) = [1.0, 3.0, 0.0]
+A(2,:) = [2.0, 2.0, 1.0]
+A(3,:) = [3.0, 1.0, 0.0]
+
+B = [1.0, 2.0, 3.0]
+
+call GaussJordan (n, A, B)
+
+contains
+
+! Solve Ax = B using Gauss-Jordan elimination
+! A gets destroyed, answer will be returned in B
+recursive subroutine GaussJordan (n, A, B)
+	integer n, i, j, k, l; real A(n,n), B(n), x(n)
+	
+	! Pivot indices
+	integer row(n), col(n), pivot(1)
+	forall (i = 1:n) row(i) = i
+	forall (j = 1:n) col(j) = j
+	! col is superfluous right now
+	
+	write (*,*) A(:,row(:))
+	write (*,*) ""
+	write (*,*) B(row(:))
+	write (*,*) ""
+	write (*,*) ""
+	
+	do j = 1,n-1
+		! If the largest number in the column is zero, go next.
+		if (maxval(abs(A(j, row(j:)))) + (j-1) /= 0) then
+			pivot = maxloc(abs(A(j, row(j:)))) + (j-1)
+			write (*,*) "Pivot:", pivot
+			! Swap rows
+			row([j,pivot]) = row([pivot,j])
+	
+			! Kill column. B first or we eliminate the element we're
+			! using to kill everything.
+			do i = j+1,n
+				B(row(i)) = B(row(i)) - &
+					(A(j,row(i))/A(j,row(j))) * B(row(j))
+				A(:,row(i)) = A(:,row(i)) - &
+					(A(j,row(i))/A(j,row(j))) * A(:,row(j))
+		
+				! See if the right operation was performed
+				write (*,*) A(:,row(:))
+				write (*,*) ""
+				write (*,*) B(row(:))
+				write (*,*) ""
+				write (*,*) ""
+			end do
+		end if
+	end do
+	
+	x(n) = B(row(n))/A(n,row(n))
+	do k = n-1,1,-1
+		x(k) = B(row(k))
+		do l = k+1,n
+			x(k) = x(k) - A(l,row(k))*x(l)
+		end do
+		x(k) = x(k)/A(k,row(k))
+	end do
+	write (*,*) x
+	
+	return
+end subroutine
+
+! Chebyshev polynomial T_n(x)
+elemental function ChebyshevT(x, n)
+	real ChebyshevT, x; integer n
+	intent(in) x, n
+	
+	ChebyshevT = cos(n*acos(x))
+end function
+
+end program
