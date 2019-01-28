5fac6e6678decb7c93a997be4ffc4ca7119e1833
PHYS395 - Computational Physics Sandbox <phys395@sandbox.phys.sfu.ca>
2019-01-28 02:37:53 -0800

    Derivatives are now done numerically, instead of being punched in properly

--- 01 Gauss Jordan/GJChebyshevLin10.f90
+++ 01 Gauss Jordan/GJChebyshevLin10.f90
@@ -7,12 +7,12 @@ integer,parameter :: n = 10, p = 10000
 ! Original Function Parameters
 real x(n), y(n,n), Exact(n), Coefficients(n), Approx
 ! Derivative Parameters
-real yP(n,n), ExactP(n), CoefficientsP(n), &
+real xP(n-1), yP(n-1,n-1), ExactP(n-1), CoefficientsP(n-1), &
 	ApproxP
 ! Fine Grid Parameters (original)
 real xF(p), ApproxF, ExactF(p), DiffF, LocF
 ! Fine Grid Parameters (derivative)
-real ApproxFP, ExactFP(p), DiffFP, LocFP
+real xFP(p-1), ApproxFP, ExactFP(p-1), DiffFP, LocFP
 ! Counters
 integer i, j, k, l, m, q
 
@@ -20,7 +20,11 @@ integer i, j, k, l, m, q
 forall (i=1:n)
 	x(i) = (2.0*i-n-1.0)/(n-1.0)
 	Exact(i) = (1.0+10.0*x(i)**2.0)**(-1.0)
-	ExactP(i) = -20.0*x(i)/((1.0+10.0*x(i)**2.0)**(2.0))
+end forall
+
+forall (i=1:n-1)
+	ExactP(i) = (Exact(i+1)-Exact(i))/(x(i+1)-x(i))
+	xP(i) = (x(i+1)+x(i))/2.0
 end forall
 
 ! Make the matrix to be solved by Gauss Jordan
@@ -28,39 +32,56 @@ end forall
 ! row
 do j=1,n
 	do k=1,n
-		y(k,j) = ChebyshevT(x(j), k-1)
-		yP(k,j) = ChebyshevT(x(j),k-1)
+		y(k,j) = ChebyshevT(x(j),k-1)
+	end do
+end do
+
+do j=1,n-1
+	do k=1,n-1
+		yP(k,j) = ChebyshevT(xP(j),k-1)
 	end do
 end do
 
 call GaussJordan (n, y, Exact, Coefficients)
-call GaussJordan (n, yP, ExactP, CoefficientsP)
+call GaussJordan (n-1, yP, ExactP, CoefficientsP)
 
 !Make a much finer grid
 forall (q=1:p)
 	xF(q) = (2.0*q-p-1.0)/(p-1.0)
 	ExactF(q) = (1.0+10.0*xF(q)**2.0)**(-1.0)
-	ExactFP(q) = -20.0*xF(q)/((1.0+10.0*xF(q)**2.0)**(2.0))
+end forall
+
+forall (q=1:p-1)
+	ExactFP(q) = (ExactF(q+1)-ExactF(q))/(xF(q+1)-xF(q))
+	xFP(q) = (xF(q+1)+xF(q))/2.0
 end forall
 
 do l=1,p
 	ApproxF = 0
-	ApproxFP = 0
 	do m=1,n
 		ApproxF = ApproxF + Coefficients(m) * &
-			ChebyshevT(xF(l),m-1)
-		ApproxFP = ApproxFP + CoefficientsP(m) * &
 			ChebyshevT(xF(l),m-1)
 	end do
 	if (abs(DiffF) < abs(ExactF(l)-ApproxF)) then
 		LocF = xF(l)
 		DiffF = ExactF(l)-ApproxF
 	end if
+	write (*,*) xF(l), ApproxF, ExactF(l)
+end do
+write (*,*) ""
+write (*,*) ""
+
+do l=1,p-1
+	ApproxFP = 0
+	do m=1,n-1
+		ApproxFP = ApproxFP + CoefficientsP(m) * &
+			ChebyshevT(xFP(l),m-1)
+	end do
 	if (abs(DiffFP) < abs(ExactFP(l)-ApproxFP)) then
-		LocFP = xF(l)
+		LocFP = xFP(l)
 		DiffFP = ExactFP(l)-ApproxFP
 	end if
-	write (*,*) xF(l), ApproxF, ExactF(l), ApproxFP, ExactFP(l)
+	write (*,*) xFP(l), ApproxFP, ExactFP(l)
 end do
 write (*,*) ""
 write (*,*) ""
